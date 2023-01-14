12d2a5ccbebbac8a1047d10488e7543aed90b88b
SaiKiran829 <saismartmudhiraj829@gmail.com>
2023-01-14 20:14:29 +0530

    first commit

---
+++ Adding List Inside a list/Adding List Inside a list/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Transactions;
+
+namespace list
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+
+        public Employee(int id, string Name)
+        {
+            this.name = Name;
+            this.Id = id;
+        }
+
+        public Employee() { }
+    }
+    public class Project
+    {
+        public string projectName { get; set; }
+        public int projectId { get; set; }
+
+        public List<Employee> employeesInEmp = new List<Employee>();
+
+        public Project(string projectName ,int projectid)
+        {
+            this.projectName = projectName;
+            this.projectId = projectid;
+        }
+    }
+
+    public class actual
+    {
+        public static void Main(string[] args)
+        {
+            List<Project> projects = new List<Project>();
+            Project project = new Project("sai",1);
+            projects.Add(project);
+
+            Project project1 = new Project("kiran", 2);
+            projects.Add(project1);
+
+            List<Employee> employees = new List<Employee>();
+            Employee employee = new Employee(1,"kiransai");
+
+            Console.WriteLine("Enter id of project");
+            int searchId = Convert.ToInt32(Console.ReadLine());
+            for(int i =0;i<projects.Count;i++) 
+            {
+                if (projects[i].projectId == searchId)
+                {
+                    projects[i].employeesInEmp.Add(employee);
+                }
+            }
+
+            int search = 1;
+            for(int i= 0; i < projects.Count; i++)
+            {
+                if (projects[i].projectId == search) 
+                { 
+                    for(int j = 0; j < projects[i].employeesInEmp.Count; j++)
+                    {
+                        Console.WriteLine(projects[i].employeesInEmp[j].name);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ BInarySearch/BInarySearch/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinarySearch
+{
+    class BinarySearch
+    {
+         public static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+            list.Add(8);
+            list.Add(9);
+            list.Add(10);
+            int f = 0;
+            int l = list.Count-1;
+            int toFind = 1;
+            int found = Searching(list, f, l, toFind);
+            if(found == -1)
+            {
+                Console.WriteLine("Not found");
+            }
+            else
+            {
+                Console.WriteLine("The item is at " + found);
+            }
+        }
+
+        public static int  Searching(List<int> list,int first,int last,int x)
+        {
+
+            if (first <= last)
+            {
+                int mid = (first + last) / 2;
+                if (list[mid] == x)
+                {
+                    return mid;
+                }
+                else if (list[mid] < x)
+                {
+
+                    return Searching(list, mid + 1, last, x);
+                }
+                else if (list[mid] > x)
+                {
+                    return Searching(list, first, mid - 1, x);
+                }
+            }
+            return -1;
+            
+        }
+    }
+}
